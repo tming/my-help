ea57c8130b32d872931aa4f5abe9aef1c2ec94c7
tomtian(田明) <tomtian@tencent.com>
2023-07-07 10:57:31 +0800

    feat:adjust version file

--- src/common/version.go
+++ src/common/version.go
@@ -1,9 +1,8 @@
 package common
 
 var (
-	Version      = "1.2.3"
-	Tag          = "2001-02-03 Release"
-	BuildTime    = "2001-02-03 4:05:06"
-	GitHash      = "unknown"
-	DisttaskRepo = ""
+	Version   = "1.2.3"
+	Tag       = "2001-02-03 Release"
+	BuildTime = "2001-02-03 4:05:06"
+	GitHash   = "unknown"
 )
